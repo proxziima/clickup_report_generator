03996753c70282872a03c478ebeccf7bc8884683
queirozvinicius <felipevs.queiroz@gmail.com>
2021-11-19 17:20:12 -0300

    :pencil: Added the csv reporting function

--- gerar_relatorio.py
+++ gerar_relatorio.py
@@ -81,28 +81,36 @@ def main():
     
     #popula o objeto tasks com as tarefas do timesheet
     for time_entry in time_entries['data']:
-        tasks[time_entry['task']['id']] = time_entry['task']['name']
+        """
+        checa se a task é bloqueada pra mim
+        se for bloqueada pula a ação no loop
+        !isso é temporário visto que estão mexendo
+        na estrutura do Clickup
+        """
+        if(time_entry['task'] != '0'):
+            tasks[time_entry['task']['id']] = time_entry['task']['name']
 
-        #insere nova linha
-        time_entries_rows.append([
-            time_entry['task']['id'],
-            time_entry['task']['name'],
-            time_entry['id'],
-            time_entry['description'],
-            pd.to_datetime(time_entry['start'], unit='ms', utc=True).tz_convert( TIMEZONE),
-            pd.to_datetime(time_entry['end'], unit='ms', utc=True).tz_convert( TIMEZONE),
-            int(time_entry['duration']),
-            time_entry['user']['id'],
-            time_entry['user']['username']
-        ])
+            #insere nova linha
+            time_entries_rows.append([
+                time_entry['task']['id'],
+                time_entry['task']['name'],
+                time_entry['id'],
+                time_entry['description'],
+                pd.to_datetime(time_entry['start'], unit='ms', utc=True).tz_convert( TIMEZONE),
+                pd.to_datetime(time_entry['end'], unit='ms', utc=True).tz_convert( TIMEZONE),
+                int(time_entry['duration']),
+                time_entry['user']['id'],
+                time_entry['user']['username']
+            ])
 
     #cria um novo dataframe
     time_entries_df = pd.DataFrame(time_entries_rows, columns=columns_time_entries)
 
-    print(f"{len(tasks)} tarefas exportadas")
+    print(f"E {len(tasks)} tarefas exportadas")
 
     # ===
     # hora de fazer iteração pra cada tarefa
+    # pega as informações da tarefa e adiciona no dicionario.
     i = 0
     for task_id in tasks.keys():
         print(f"> {str(i+1).rjust(3)}/{len(tasks)} | Buscando info sobre a tarefa {task_id}: {tasks[task_id]}")
@@ -135,6 +143,10 @@ def main():
     #junt os dois dataframes
     all_df = pd.merge(time_entries_df, lists_df, how="left", on='task_id')
 
+    #cria uma nova coluna horas
+    #.assign() para criar nova coluna
+    all_df = all_df.assign(hours = lambda x: x.time_duration / (3600 * 1000))
+
     grouped_by_user = all_df.groupby('user_name')
 
     for username, user_frame in grouped_by_user:
@@ -153,7 +165,37 @@ def main():
             print(f"### {project.ljust(20)} | {time_spent:4.1f} hrs | {fraction:6.2%}")
 
         #exporta para formato csv dentro da pasta data
-        tasks_filename = f"{username}"
+        tasks_filename = f"{username} - tarefas {util.timestamp_to_human(start_ts)} - {util.timestamp_to_human(end_ts)}.csv"
+        tasks_group['hours'].sum().rename_axis(['Project', 'Task']).reset_index().to_csv(f"./report/{tasks_filename}")
+
+        print(f"\n O detalhamento das tarefas foi salva em: {tasks_filename}")
+
+        """
+        Preenche as datas faltantes.
+        O date_range gera o período de datas entre
+        o start e o end informados.
+        """
+        missing_dates = pd.date_range(start, util.get_end_date(end_ts), tz=TIMEZONE)
+        missing_str = ['test' for _ in range(len(missing_dates))]
+        missing_num = [0 for _ in range(len(missing_dates))]
+        missing_empty = [0 for _ in range(len(missing_dates))]
+
+        missing_df = pd.DataFrame({ 'task_id': missing_str, 'task_name': missing_str,
+                            'time_id': missing_str, 'time_start': missing_dates, 
+                            'time_end': missing_dates, 'time_duration': missing_num, 
+                            'list_id': missing_str, 'list_name': missing_empty,
+                            'user_id': missing_str, 'user_name': missing_str, 'hours': missing_empty})
+        full_frame = user_frame.append(missing_df)
+
+        timesheet_df = pd.pivot_table(full_frame, index=['list_name'], margins= True, margins_name='Total', columns='time_start', values=['hours'], aggfunc=[np.sum], fill_value='')
+
+        timesheet_filename = f"{ username } - timesheet { util.timestamp_to_human( start_ts ) } - { util.timestamp_to_human( end_ts) }.csv"
+
+        timesheet_df.to_csv(f"./report/{ timesheet_filename }")
+
+        print( f"\n====== TIMESHEET ======" )
+        print( timesheet_df )
+        print( f"The timesheet is saved to: {timesheet_filename}" )
 
 # Função para pegar os membros do time
 def get_team_members():
